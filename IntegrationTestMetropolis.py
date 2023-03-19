bfaf03ea7a58318b87a6f8d8399a004d4ce9fdb1
xnatalienguyen <121984393+xnatalienguyen@users.noreply.github.com>
2023-03-18 23:45:06 -0700

    Update IntegrationTestMetropolis.py
    Add python file of IntegrationTestMetropolis

--- IntegrationTestMetropolis.py
+++ IntegrationTestMetropolis.py
@@ -72,7 +72,7 @@ def fit_roc_bayesian(sdtList):
 
 
 # Define the number of SDT trials and generate a simulated dataset
-sdtList = SignalDetection.simulate(dprime=1,
+sdtList = SignalDetection.simulate(dPrime=1,
                                    criteriaList=[-1, 0, 1],
                                    signalCount=40,
                                    noiseCount=40)
