2cb7a54e32caadc74b366d39796117a6a26e5e3d
xnatalienguyen <121984393+xnatalienguyen@users.noreply.github.com>
2023-03-18 23:44:48 -0700

    Update Metropolis.py
    Add Python file of Metropolis

--- Metropolis.py
+++ Metropolis.py
@@ -1,45 +1,57 @@
 import numpy as np
-from statistics import NormalDist
 
 
 class Metropolis:
-    def __init__(self, logTarget, initialState):
+    def __init__(self, logTarget, initialState, stepSize=1.0):
         self.logTarget = logTarget
-        self.state = initialState
-        self.accepted = 0
-        self.proposed = 0
+        self.currentState = initialState
+        self.stepSize = stepSize
+        self.acceptanceRate = None
         self.samples = []
 
-    def accept(self, proposal):
-        acceptance_prob = min(1, np.exp(self.logTarget(
-            proposal) - self.logTarget(self.state)))
-        self.proposed += 1
-        if acceptance_prob > np.random.uniform():
-            self.state = proposal
-            self.accepted += 1
+    def _accept(self, proposal):
+        logProposal = self.logTarget(proposal)
+        logCurrent = self.logTarget(self.currentState)
+        logAlpha = logProposal - logCurrent
+        if np.log(np.random.rand()) < logAlpha:
+            self.currentState = proposal
             return True
         else:
             return False
 
     def adapt(self, blockLengths):
-        for block in range(len(blockLengths)):
-            for i in range(blockLengths[block]):
-                proposal = np.random.normal(self.state, self.stdDev)
-                self.accept(proposal)
-            acceptance_rate = self.accepted / self.proposed
-            if acceptance_rate < 0.1:
-                self.stdDev /= 2
-            elif acceptance_rate > 0.3:
-                self.stdDev *= 2
+        for blockLength in blockLengths:
+            accepts = 0
+            proposals = 0
+            for i in range(blockLength):
+                proposal = np.random.normal(
+                    loc=self.currentState, scale=self.stepSize)
+                if self._accept(proposal):
+                    accepts += 1
+                proposals += 1
+            acceptanceRate = accepts / proposals
+            if acceptanceRate > 0.5:
+                self.stepSize *= 1.1
+            else:
+                self.stepSize *= 0.9
+            self.acceptanceRate = acceptanceRate
+        return self
 
-    def sample(self, n):
-        for i in range(n):
-            proposal = np.random.normal(self.state, self.stdDev)
-            if self.accept(proposal):
-                self.samples.append(self.state)
+    def sample(self, nSamples):
+        self.samples = [self.currentState]
+        for i in range(nSamples):
+            proposal = np.random.normal(
+                loc=self.currentState, scale=self.stepSize)
+            if self._accept(proposal):
+                self.samples.append(proposal)
+            else:
+                self.samples.append(self.currentState)
+        return self
 
     def summary(self):
+        n = len(self.samples)
         mean = np.mean(self.samples)
-        ci = NormalDist().inv_cdf(0.975) * np.std(self.samples) / \
-            np.sqrt(len(self.samples))
-        return {"mean": mean, "95% CI": (mean - ci, mean + ci)}
+        std = np.std(self.samples, ddof=1)
+        ci025 = np.percentile(self.samples, 2.5)
+        ci975 = np.percentile(self.samples, 97.5)
+        return {'mean': mean, 'std': std, 'c025': ci025, 'c975': ci975}
